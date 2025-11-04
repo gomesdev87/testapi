25215ba7fe0f32d7a5d033757f0f92d7dd724795
gomesdev87 <134112503+gomesdev87@users.noreply.github.com>
2025-11-04 12:04:48 +0000

    inicializetion

---
+++ src/test/java/xyz/gomesweb87/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.gomesweb87.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
